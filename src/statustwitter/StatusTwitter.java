a7a8b70b806912c82d78cf72d6e805d491699202
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-19 09:52:31 +0100

    hacemos otro cambio en esta rama

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -31,10 +31,10 @@ twitter = new TwitterFactory(cb.build()).getInstance();
 
 Paging pagina = new Paging();
      
-        Twitter andy = new TwitterFactory().getInstance();
+        Twitter andy10 = new TwitterFactory().getInstance();
  
         Query query = new Query("#Cangas");
-        QueryResult result = andy.search(query);
+        QueryResult result = andy10.search(query);
         for (Status status : result.getTweets()) {
             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
         }
